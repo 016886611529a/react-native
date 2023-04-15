18e5388b6c8bb8b7e601a93acab5cc6130ce3e7f
longnn <nguyenngoclongpdl13@gmail.com>
2023-04-15 12:01:54 +0700

    kkk

---
+++ components/UIButton.js
@@ -0,0 +1,43 @@
+import {
+    Text,
+    Alert,
+    TouchableOpacity,
+  } from 'react-native';
+import React from 'react';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+import { colors } from '../constants';
+function UIButton(props) {
+    const {onPress,title, isSelected} = props;
+  return (
+    <TouchableOpacity
+      onPress={onPress
+      }
+      style={{
+        borderColor: 'white',
+        borderWidth: 1,
+        borderRadius: 5,
+        height: 45,
+        marginVertical: 20,
+        marginHorizontal: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: isSelected ? 'white' : 'transparent',
+      }}>
+      {isSelected && <Icon
+        name={'check-circle'}
+        style={{
+          color: 'green',
+          fontSize: 20,
+          position: 'absolute',
+          left: 10,
+          top: 12,
+        }}
+      />}
+      <Text style={{
+        
+         color: isSelected == true? colors.primary : 'white' , fontSize: 15}}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+export default UIButton;
--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,10 @@
 import images from "./images";
 import icons from "./icons";
+import colors from "./colors";
+import fontSizes from "./fontSizes";
 export {
     icons,
-     images
+     images,
+     colors,
+     fontSizes
 }
